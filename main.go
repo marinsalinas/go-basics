722be90fdb34bb7a93029540c691d5408a5e9579
Marin Salinas <marinssalinas@gmail.com>
2018-08-13 16:41:53 -0500

    add comments to hello world

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
+/*A package definition is always
+the very first thing in a Go source file.
+*/
 package main
 
+//Package used by the program must be explicity
+//imported after the package definition
 import "fmt"
 
+//The main() function is the entry point for all
+//Go programs. It must have this name.
 func main() {
 	fmt.Println("Hello world 🐹 !")
 }
