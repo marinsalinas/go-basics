560b2601a8e5d753da43dfdfa7bdb8b756874be0
Marin Salinas <marinssalinas@gmail.com>
2018-08-15 16:31:46 -0500

    adding goroutines with waitgroups

--- main.go
+++ main.go
@@ -1,15 +1,40 @@
+// This sample program demonstrates how to create goroutines and
+// how the goroutine scheduler behaves with three logical processors.
+
 package main
 
 import (
 	"fmt"
-
-	"github.com/marinsalinas/go-basics/model"
+	"math"
+	"runtime"
+	"sync"
 )
 
 func main() {
-	jumpers := model.GetList()
-	for i := range jumpers {
-		//Print with format.
-		fmt.Printf("%+v\n", jumpers[i])
+	// Allocate three logical processors for the scheduler to use.
+	runtime.GOMAXPROCS(3)
+
+	var wg sync.WaitGroup
+	names := []string{"Marín", "Berny", "Tony"}
+
+	//Add a count depends on the length of names array
+	wg.Add(len(names))
+
+	for _, name := range names {
+		//Anonymous function.
+		go func(name string) {
+			//defers the execution of a function until
+			//the surrounding function returns.
+			defer wg.Done()
+			r := 0.0
+			for i := 0; i < 100000000; i++ {
+				r += math.Pi * math.Sin(float64(len(name)))
+			}
+			fmt.Println("Hello: ", name)
+		}(name)
+
 	}
+
+	// Wait for the goroutines to finish.
+	wg.Wait()
 }
